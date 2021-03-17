081e1be3bd42d355c3c2982d6025e5515463d51c
Mashu <you@example.com>
2021-03-16 22:07:59 -0400

    index.html update
    Se actualizo el carusel y se agragaron 2 slide

--- src/js/index.js
+++ src/js/index.js
@@ -4,13 +4,31 @@
 
 const presst = document.querySelector('#presentacion')
 const cardContainer = document.querySelector('#card-container')
+const scrollToSlide1 = document.querySelector('#btm-1');
+const slide2 = document.querySelector('#btm-2');
+
+
+$('#categoriesButton').click('slide.bs.carousel', function () {
+    $('#myCarousel').carousel(1);
+})
+
+// location.href = '#carouselExampleControls';
+$('#productButton').click('slide.bs.carousel', function () {
+    $('#myCarousel').carousel(2);
+})
+
+$('#home').click('slide.bs.carousel', function () {
+    $('#myCarousel').carousel(0);
+})
+
+
 
 window.onload = () => {
     // Funcion para estar pendiente cuando se hace click sobre el boton de las categorias
     const categoryButton = document.getElementById('categoriesButton');
-    categoryButton.addEventListener('click', () => {
-        location.href = 'src/pages/categories.html';
-    });
+    // location.href = '#carouselExampleControls';
+
+
     const dataButton = document.getElementById('dataButton');
     dataButton.addEventListener('click', () => {
         // location.href = '#carouselExampleControls';
@@ -20,12 +38,13 @@ window.onload = () => {
         presst.classList.add('fade-out');
         cardContainer.classList.add('fade-in');
     });
+
     // Funcion para estar pendiente cuando se hace click sobre el boton de las categorias
     // Funcion para estar pendiente cuando se hace click sobre el boton de los productos
     const productButton = document.getElementById('productButton');
-    productButton.addEventListener('click', () => {
-        location.href = 'src/pages/products.html';
-    });
+    // productButton.addEventListener('click', () => {
+    //     location.href = 'src/pages/products.html';
+    // });
     // Funcion para estar pendiente cuando se hace click sobre el boton de los productos
 
 }
