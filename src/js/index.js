53e0cabee6d3a0ab78d36ea79166c948b3ef3522
Mashu <you@example.com>
2021-03-19 20:49:24 -0400

    Yet Another Commit

--- src/js/index.js
+++ src/js/index.js
@@ -7,6 +7,9 @@ const cardContainer = document.querySelector('#card-container')
 const scrollToSlide1 = document.querySelector('#btm-1');
 const slide2 = document.querySelector('#btm-2');
 
+$('#home').click('slide.bs.carousel', function () {
+    $('#myCarousel').carousel(0);
+})
 
 $('#categoriesButton').click('slide.bs.carousel', function () {
     $('#myCarousel').carousel(1);
@@ -17,27 +20,11 @@ $('#productButton').click('slide.bs.carousel', function () {
     $('#myCarousel').carousel(2);
 })
 
-$('#home').click('slide.bs.carousel', function () {
-    $('#myCarousel').carousel(0);
-})
-
-
 
 window.onload = () => {
     // Funcion para estar pendiente cuando se hace click sobre el boton de las categorias
     const categoryButton = document.getElementById('categoriesButton');
     // location.href = '#carouselExampleControls';
-
-
-    const dataButton = document.getElementById('dataButton');
-    dataButton.addEventListener('click', () => {
-        // location.href = '#carouselExampleControls';
-        $('#myCarousel').click('slide.bs.carousel', function () {
-            $('#myCarousel').carousel(1);
-        })
-        presst.classList.add('fade-out');
-        cardContainer.classList.add('fade-in');
-    });
 
     // Funcion para estar pendiente cuando se hace click sobre el boton de las categorias
     // Funcion para estar pendiente cuando se hace click sobre el boton de los productos
